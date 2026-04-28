6bef02740e60f6decb3c547187af61ff848df37a
lina zghal <linazghal65@gmail.com>
2026-04-28 02:51:25 +0100

    modifier le code

--- consolidé/src/com/soukOnline/models/lina code/Livraison.java
+++ consolidé/src/com/soukOnline/models/lina code/Livraison.java
@@ -5,17 +5,17 @@ public class Livraison {
     private String destination;
     private String statut; // EN_PREPARATION, EXPEDIE, LIVRE, RETOUR
 
-    // Constructeur vide
+    
     public Livraison() {}
 
-    // Constructeur complet
+ 
     public Livraison(String codeSuivi, String destination) {
         this.codeSuivi = codeSuivi;
         this.destination = destination;
         this.statut = "EN_PREPARATION";
     }
 
-    // Méthode métier : Mise à jour du statut (Centralisation logistique)
+   
     public void mettreAJourStatutLivraison(String nouveauStatut) {
         this.statut = nouveauStatut;
         System.out.println("--- Hub Logistique SoukOnline ---");
@@ -30,7 +30,7 @@ public class Livraison {
         System.out.println("Suivi : " + codeSuivi + " | Destination : " + destination + " | État : " + statut);
     }
 
-    // Getters et Setters
+  
     public String getCodeSuivi() { return codeSuivi; }
     public void setCodeSuivi(String codeSuivi) { this.codeSuivi = codeSuivi; }
 
