478ac379ec342c3af3ea1a62316c6434cf7d3555
Kader201103 <abdelkaderjlassi86@gmail.com>
2026-04-28 03:09:13 +0100

    dashboard

--- consolidé/src/com/soukOnline/models/kader code/Dashboard.java
+++ consolidé/src/com/soukOnline/models/kader code/Dashboard.java
@@ -1,41 +1,25 @@
 package com.soukOnline.models;
 
 public class Dashboard {
-    private int idDashboard;
     private String nomEcommercant;
 
-    // Constructeur vide
-    public Dashboard() {}
-
-    // Constructeur complet
-    public Dashboard(int idDashboard, String nomEcommercant) {
-        this.idDashboard = idDashboard;
+    public Dashboard(String nomEcommercant) {
         this.nomEcommercant = nomEcommercant;
     }
 
-    // Méthode métier : Afficher l'évolution (Simulation graphique)
-    public void afficherEvolutionVentes() {
+
+    public void genererRapportDynamique(Finance f) {
         System.out.println("--- Tableau de Bord SoukOnline : " + nomEcommercant + " ---");
-        System.out.println("Graphique d'évolution des ventes chargé...");
-        System.out.println("Lundi: [|
-Spoiler
-] 500 DT");
-        System.out.println("Mardi: [|
-Spoiler
-|] 800 DT");
-        System.out.println("Mercredi: [
-Spoiler
-||] 400 DT");
-        System.out.println("Tendance : Hausse de 15% par rapport à la semaine dernière.");
+        
+    
+        double profitActuel = f.calculerMargeNette(); 
+        
+        System.out.println("État financier actuel : " + profitActuel + " DT"); 
+        
+        if (profitActuel > 0) {
+            System.out.println("Performance : Votre boutique est actuellement rentable.");
+        } else {
+            System.out.println("Alerte : Les dépenses dépassent les revenus !");
+        }
     }
-
-    // Méthode métier : Générer rapport
-    public void genererRapportJournalier() {
-        System.out.println("Génération du rapport PDF en cours...");
-        System.out.println("Rapport prêt : Ventes totales du jour = 1700 DT | Colis livrés = 12");
-    }
-
-    // Getters et Setters
-    public String getNomEcommercant() { return nomEcommercant; }
-    public void setNomEcommercant(String nomEcommercant) { this.nomEcommercant = nomEcommercant; }
 }
